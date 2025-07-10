46adc8e5184ceef4cc63916f923d4d085f73f3b8
APaud3l <aamod.paudel@coderacademy.edu.au>
2025-07-10 21:10:09 +1000

    feat: Database connection, CLI commands, Model creation

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+#                                      database+driver://username:password@server:port/databasename
+
+app.config['SQLALCHEMY_DATABASE_URI']="postgresql+psycopg2://mar_user:123456@localhost:5432/mar_ecommerce"
+
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    # Define table name
+    __tablename__ = "products"
+    # Define primary key
+    id = db.Column(db.Integer, primary_key=True)
+    # Define non-key attributes
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255))
+    price = db.Column(db.Float)
+    stock = db.Column(db.Integer)
+
+@app.cli.command("create") # flask create
+def create_table():
+    db.create_all()
+    print("Tables created!")
+
+@app.cli.command("drop") # flask drop
+def drop_tables():
+    db.drop_all()
+    print("Tables dropped!")
+
+@app.cli.command("seed") # flask seed
+def seed_tables():
+    # Create an instance of products
+    product1 = Product(
+        name = "Product 1",
+        description = "This is Product 1",
+        price = 12.99,
+        stock = 5
+    )
+
+    product2 = Product()
+    product2.name = "Product 2"
+    product2.price = 15
+    product2.stock = 0
+
+    # Like Git operations, we need to add and commit
+    db.session.add(product1)
+    db.session.add(product2)
+    
+    db.session.commit()
+
+    print("Table seeded.")
